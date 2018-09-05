e3517730087236aedecf4f91f27bf34a6af7d4bb
GABRIELROLAND <32078927+GABRIELROLAND@users.noreply.github.com>
2018-09-05 19:56:53 -0300

    Add files via upload

---
+++ aula6.php
@@ -0,0 +1,18 @@
+<?php
+
+//Condiçao While
+$c=1;
+while($c<=10){
+    
+    echo'$c;';
+    $c++;
+}
+    echo'<br>';
+    echo'<hr>';
+//contagem Regressiva
+$c=10;
+while($c>=1){
+    
+    echo'$c;';
+    $c--;
+}
